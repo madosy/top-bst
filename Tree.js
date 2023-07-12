e742f3e4415082452eb39f748e53f3fbbced6637
Dasom Heo <dasomee@outlook.com>
2023-07-12 18:21:56 -0400

    Tree.delete() and Tree.find() added

--- Tree.js
+++ Tree.js
@@ -1,6 +1,7 @@
 import Node from "./Node.js";
 import mergeSort from "./mergeSort.js";
 import removeDuplicates from "./removeDuplicates.js";
+import prettyPrint from "./prettyPrint.js";
 
 class Tree {
   constructor(array) {
@@ -24,10 +25,80 @@ class Tree {
     return root;
   }
 
-  insert() {
-    //check the data; if greater, move to right. else move to left.
-    //if left/right is null, create a node there.
+  insertAt(root, value) {
+    if (root.data == value) return;
+
+    if (root.data < value) {
+      if (root.right == null) return (root.right = new Node(value));
+      else this.insertAt(root.right, value);
+    } else {
+      if (root.left == null) return (root.left = new Node(value));
+      else this.insertAt(root.left, value);
+    }
   }
-  delete() {}
+
+  insert(value) {
+    this.insertAt(this.root, value);
+    prettyPrint(this.root);
+  }
+
+  getSmallestNode(root) {
+    if (root.left !== null) return this.getSmallestNode(root.left);
+    else return root;
+  }
+
+  deleteAt(root, value, parent = null) {
+    if (root == null) return;
+
+    const isLeafNode = root.left == null && root.right == null;
+    const hasTwoChild = root.left !== null && root.right !== null;
+    const hasOneChild =
+      (root.left !== null || root.right !== null) && !hasTwoChild;
+
+    if (root.data == value) {
+      if (isLeafNode) {
+        parent.right !== null && parent.right.data == root.data
+          ? (parent.right = null)
+          : (parent.left = null);
+      }
+      if (hasOneChild) {
+        parent.right !== null && parent.right.data == root.data
+          ? (parent.right = root.left || root.right)
+          : (parent.left = root.left || root.right);
+      }
+      if (hasTwoChild) {
+        let nextBiggestNode = this.getSmallestNode(root.right);
+        this.delete(nextBiggestNode.data);
+        nextBiggestNode.left = root.left;
+        nextBiggestNode.right = root.right;
+
+        if (parent !== null) {
+          parent.right.data == root.data
+            ? (parent.right = nextBiggestNode)
+            : (parent.left = nextBiggestNode);
+        } else {
+          this.root = nextBiggestNode;
+        }
+      }
+      return;
+    }
+    if (root.data > value) return this.deleteAt(root.left, value, root);
+    if (root.data < value) return this.deleteAt(root.right, value, root);
+  }
+
+  delete(value) {
+    this.deleteAt(this.root, value);
+  }
+
+  findAt(root, value) {
+    if (root == null) return null;
+    if (root.data == value) return root;
+    if (root.data > value) return this.findAt(root.left, value);
+    if (root.data < value) return this.findAt(root.right, value);
+  }
+
+  find(value) {
+    return this.findAt(this.root, value);
+  }
 }
 export default Tree;
--- main.js
+++ main.js
@@ -1,13 +1,10 @@
 import sortedArrayToBST from "./sortedArrayToBST.js";
 import Tree from "./Tree.js";
-
 import prettyPrint from "./prettyPrint.js";
 
-console.log("hello");
-
-let test1 = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+let test1 = [1, 7, 4, 6, 10, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 let test2 = [1, 2, 3, 4, 5, 6, 7];
 
-let tree = new Tree(test2);
+let tree = new Tree(test1);
 console.log(tree);
 prettyPrint(tree.root);
